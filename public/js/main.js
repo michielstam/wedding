0321c395da1a17bf4f63a02eef2177d559c48ebd
Michiel Stam <mbstam@gmail.comm>
2015-10-13 01:15:42 +0200

    tijdelijke oplossing

--- public/js/main.js
+++ public/js/main.js
@@ -44,46 +44,16 @@ $(document).ready(function (){
     content:  '<div class="info"><strong>This is my company</strong><br><br>My company address is here<br> 32846 Sydney</div>'
   });
 
-  // Popover 
-$('#registerHere input').hover(function()
-{
-$(this).popover('show')
+
+  $("button#submit").click(function(){
+		
+		var p = window.createPopup()
+		  var pbody = p.document.body
+		  pbody.style.backgroundColor = "lime"
+		  pbody.style.border = "solid black 1px"
+		  pbody.innerHTML = "This is a pop-up! Click outside to close."
+		  p.show(150,150,200,50,document.body)
+		
+	});
 });
 
- // Validation
-$("#registerHere").validate({
-rules:{
-user_name:"required",
-user_email:{required:true,email: true},
-pwd:{required:true,minlength: 6},
-cpwd:{required:true,equalTo: "#pwd"},
-gender:"required"
-},
-
-messages:{
-user_name:"Enter your first and last name",
-user_email:{
-required:"Enter your email address",
-email:"Enter valid email address"},
-pwd:{
-required:"Enter your password",
-minlength:"Password must be minimum 6 characters"},
-cpwd:{
-required:"Enter confirm password",
-equalTo:"Password and Confirm Password must match"},
-gender:"Select Gender"
-},
-
-errorClass: "help-inline",
-errorElement: "span",
-highlight:function(element, errorClass, validClass)
-{
-$(element).parents('.control-group').addClass('error');
-},
-unhighlight: function(element, errorClass, validClass)
-{
-$(element).parents('.control-group').removeClass('error');
-$(element).parents('.control-group').addClass('success');
-}
-});
-});
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+var connectionString = process.env.DATABASE_URL || 'postgres://prmgswwteyqepp:O0Wu5eZhZKEzrtdwu7iMpi2cMe@ec2-54-197-241-239.compute-1.amazonaws.com:5432/d4ulen39c8bnhs';
 
+/*-- 'postgres://localhost:5432/wedding'; --*/
 
 
 /* GET home page. */
@@ -10,8 +11,51 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-//write
-router.post('/api/v1/todos', function(req, res) {
+router.route('/book')
+  .get(function(req, res) {
+    res.send('Get a random book');
+  })
+  .post(function(req, res) {
+    res.send('Add a book');
+  })
+  .put(function(req, res) {
+    res.send('Update the book');
+  });
+
+router.route('/aanmelden').post(function(req,res) {
+	
+	var results = [];
+	
+	var data = {name: req.body.name, email: req.body.email, kerk: req.body.kerk, receptie: req.body.receptie, feest: req.body.feest, aantal: req.body.aantal};
+	
+	
+	pg.connect(connectionString, function(err, client, done) {
+		// Handle connection errors
+		if(err) {
+			done();
+			console.log(err);
+			return res.status(500).json({ succes: false, data: err});
+		}
+		// SQL Query > Insert Data
+		var query = client.query("INSERT INTO dbo.gasten(name, email, kerk, receptie, feest, aantal) values($1, $2, $3, $4, $5, $6)", [data.name, data.email, data.kerk, data.receptie, data.feest, data.aantal]);
+		
+		query.on('end', function() {
+			done();
+			return res.send({"data":[
+			    {"succes": true, "message":""}
+			]});			
+		});
+	})
+});
+
+// POST method route
+router.post('/test', function (req, res) {
+  	return res.status(200),json({"data":[
+	    {"succes": true, "message":"Doe"}
+	]})	
+});
+
+router.get('/gasten', function(req, res) {
 
     var results = [];
 
@@ -20,12 +64,15 @@ router.post('/api/v1/todos', function(req, res) {
 
     // Get a Postgres client from the connection pool
     pg.connect(connectionString, function(err, client, done) {
-
-        // SQL Query > Insert Data
-        client.query("INSERT INTO items(text, complete) values($1, $2)", [data.text, data.complete]);
+        // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+          return res.status(500).json({ success: false, data: err});
+        }
 
         // SQL Query > Select Data
-        var query = client.query("SELECT * FROM items ORDER BY id ASC");
+        var query = client.query("SELECT * FROM dbo.gasten");
 
         // Stream results back one row at a time
         query.on('row', function(row) {
@@ -34,46 +81,10 @@ router.post('/api/v1/todos', function(req, res) {
 
         // After all data is returned, close connection and return results
         query.on('end', function() {
-            client.end();
+            done();
             return res.json(results);
         });
-
-        // Handle Errors
-        if(err) {
-          console.log(err);
-        }
-
     });
 });
-
-//read
-router.get('/api/v1/todos', function(req,res) {
-	
-	var results = [];
-	
-	// Get a Postgres client from connection pool
-	pg.connect(connectionString, function(err, client, done) {
-		
-		// SQL Query > Select Data
-		var query = client.query("SELECT * FROM items ORDER BY id ASC;")
-		
-		// Stream results back one row at a time
-		query.on('row', function(row) {
-			results.push(row);
-		});
-		
-		// After all data is returned, close connection
-		query.on('end', function() {
-			client.end();
-			return res.json(results);
-		});
-		
-		// Handle Errors
-		if(err) {
-			console.log(err);
-		}
-	});
-});
-
 
 module.exports = router;
